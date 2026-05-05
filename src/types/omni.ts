2934759735edf8ab953f96f8043fc3982c168ffb
erendonmez <139468016+erendonmez06@users.noreply.github.com>
2026-05-05 08:43:04 +0300

    Delete src directory

--- src/types/omni.ts
+++
@@ -1,6 +0,0 @@
-export type ConversionTask = "emotion" | "gender_age" | "speaker_clone" | "singing";
-
-export interface ConversionResult {
-  outputPath: string;
-  metrics: Record<string, number>;
-}
